3e35acefe7d78220c49bfa52c1f3718b794c3eac
sreeshanth munupally <sreeshanthmunupally@gmail.com>
2026-06-10 12:39:27 +0530

    modified main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from database import engine
 from models import Base
+from routes import donor, request
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="PulseLink API")
+
+app.include_router(donor.router)
+app.include_router(request.router)
 
 
 @app.get("/")
