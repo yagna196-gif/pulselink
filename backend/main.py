0a370da37c1dabef04edfacc7bda6adde07cc1ae
sreeshanth munupally <sreeshanthmunupally@gmail.com>
2026-06-10 10:27:37 +0530

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from database import engine
+from models import Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="PulseLink API")
 
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to PulseLink API"}
+    return {"message": "PulseLink Backend Connected"}
