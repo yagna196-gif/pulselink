3553c23eb05ce51b9ee3df4c0abd16f99bd09dc0
sreeshanth munupally <sreeshanthmunupally@gmail.com>
2026-06-10 12:28:40 +0530

    added files and data into donor.py

---
+++ backend/routes/donor.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from database import SessionLocal
+from models import Donor
+from schemas import DonorCreate
+
+router = APIRouter(prefix="/donors", tags=["Donors"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/register")
+def register_donor(donor: DonorCreate, db: Session = Depends(get_db)):
+    new_donor = Donor(
+        name=donor.name,
+        phone=donor.phone,
+        address=donor.address,
+        blood_group=donor.blood_group,
+        last_donation_date=donor.last_donation_date,
+        availability_status=donor.availability_status,
+    )
+
+    db.add(new_donor)
+    db.commit()
+    db.refresh(new_donor)
+
+    return {
+        "message": "Donor registered successfully",
+        "donor": new_donor
+    }
+
+
+@router.get("/")
+def get_donors(db: Session = Depends(get_db)):
+    donors = db.query(Donor).all()
+    return donors
