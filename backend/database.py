6826683d6319db5560ef9caf812796b6805c5005
Yagna Sree <yagna196@gmail.com>
2026-06-13 12:01:11 +0530

    support sqlite deployment

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,12 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else {}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
