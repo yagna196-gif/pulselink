41dc09b718adbbf796cbee72aba7e74ce5c51456
Yagna Sree <yagna196@gmail.com>
2026-06-13 12:23:50 +0530

    Enable deployed frontend access

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="PulseLink API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
